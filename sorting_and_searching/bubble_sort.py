e9b5db4d9e2db0f835ffed0ba5ab4c843054b401
nat-g <nata.gnn@gmail.com>
2018-03-22 17:45:43 +1100

    Create bubble_sort.py

---
+++ sorting_and_searching/bubble_sort.py
@@ -0,0 +1,9 @@
+def bubble_sort(arr):
+    # For every element (arranged backwards)
+    for n in range(len(arr)-1,0,-1):
+        for k in range(n):
+            # Switching point
+            if arr[k]>arr[k+1]:
+                temp = arr[k]
+                arr[k] = arr[k+1]
+                arr[k+1] = temp
